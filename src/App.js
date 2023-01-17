4dd3b391006640c027bd4de3288d15a816109edb
Chouteau <tetyana.chouteau@passculture.app>
2023-01-17 14:36:00 +0100

    PreparationCode

--- src/App.js
+++ src/App.js
@@ -1,72 +1,31 @@
-import "./styles.css";
 import * as React from "react";
-import { BrowserRouter as Router, Link, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./styles.css";
 
-/* Instructions 
-  Create two Routes so that the given navigation works.
+/* reactchallenges.live */
 
-  When the user navigates to /Ram, they should see "Student: Ram".
-  When the user navigates to /ids/1, they should see "Ids : 1".
-*/
+/* Instruction: 
+Implment a catch route for all routes which are not under defined routes scope*/
 
-const Employee = () => {
-
-    let { name } = useParams();
-
-    return (
-        <div className="display">
-            <h3>Employee: {name}</h3>
-        </div>
-    );
-};
-
-const Ids = () => {
-    let { id } = useParams();
-
-    return (
-        <div className="display">
-            <h3>Ids : {id}</h3>
-        </div>
-    );
-};
+const Home = () => <h1>Home</h1>;
+const Settings = () => <h1>Settings</h1>;
 
 export default function App() {
-    return (
-        <Router>
-            <h2>Employee</h2>
-            <ul>
-                <li>
-                    <Link to="/ram">Ram</Link>
-                </li>
-                <li>
-                    <Link to="/rakshman">Lakshman</Link>
-                </li>
-                <li>
-                    <Link to="/bheem">Bheem</Link>
-                </li>
-            </ul>
-            <h2>Id's</h2>
-            <ul>
-                <li>
-                    <Link to="/Ids/1">1</Link>
-                </li>
-                <li>
-                    <Link to="/Ids/2">2</Link>
-                </li>
-                <li>
-                    <Link to="/Ids/3">3</Link>
-                </li>
-                <li>
-                    <Link to="/Ids/4">4</Link>
-                </li>
-            </ul>
+  return (
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/settings">Settings</Link>
+        </li>
+      </ul>
 
-            <hr />
-
-            <Routes>
-                <Route path="/:name" element={<Employee />} />
-                <Route path="/Ids/:id" element={<Ids />} />
-            </Routes>
-        </Router>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
 }
